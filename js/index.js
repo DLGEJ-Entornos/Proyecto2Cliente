754796786c8c444cbae1c7a36c097798d82a42ff
DLGEJ-Entornos <delaguia19905@iesmarenostrum.com>
2020-02-08 21:11:17 +0100

    haciendo el login-registro desplegable

--- js/index.js
+++ js/index.js
@@ -1,13 +1,29 @@
-console.log($('#landingTools')[0]);
+function openLoginRegis(opcion) {
+  $( "#loginReg" ).animate({
+    width: "20em",
+    height: '20em',
+    left: '-80%'
+    //background-color: red,
+    //opacity: 0.4,
+    //marginLeft: "0.6in",
+    //fontSize: "3em",
+    //borderWidth: "10px"
+  }, 500 );
 
-function openLogin() {
+  function create(opcion) { //CREACION DINAMICA DEL DOM
+    var inpNom = document.createElement('input');
+    var inpPass = document.createElement('input');
+    var confirm = document.createElement('button');
+
+    if (opcion == 'log') {
+      inpNom.placeholder = 'test';       
+      document.getElementById('loginReg').appendChild(inpNom);
+
+    }else{
+
+    }
+  }
+  
+  opcion == 'log' ? create('log') : create('regis');
 }
 
-//$(function() {
-//  $('.example-popover').popover({
-//    container: 'body'
-//  })
-//})
-$(document).ready(function() {
-  $('[data-toggle="popover"]').popover()
-})
